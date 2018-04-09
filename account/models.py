f9eb7b4772c6270d30b2fb8fe06d12f69ffd561b
solon <milimi974@gmail.com>
2018-04-10 00:32:25 +0200

    add account model

--- account/models.py
+++ account/models.py
@@ -1,5 +1,16 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+from product.models import Product
+
+
 class UserProduct(models.Model):
-    pass
+    # settings.AUTH_USER_MODEL identify current login user
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.OneToOneField(Product,related_name='product', on_delete=models.CASCADE, null=True)
+    substitutions = models.ManyToManyField(Product, related_name='substitution')
+
+    def __str__(self):
+        return '{} : {}'.format(self.user ,self.product)
