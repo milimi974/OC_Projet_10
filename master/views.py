3eeb888a9c03659fcec70c50f2dd50dd24e1dd42
solon <milimi974@gmail.com>
2018-04-09 23:47:44 +0200

    add static files and main templates

---
+++ master/views.py
@@ -0,0 +1,20 @@
+# Homepage view
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'pages/index.html',{'page':'home'})
+
+
+# Mentions legal view
+def mentions(request):
+    title = "Mentions légales"
+    return render(request, 'pages/mentions_legal.html', {'title':title})
+
+
+# Contact view
+def contact(request):
+    title = "Contact"
+    return render(request, 'pages/contact.html', {'title':title})
+
+
