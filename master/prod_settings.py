cbad83c7cb8963ccd53c173db2dc1540e6b5d386
solon <milimi974@gmail.com>
2018-04-09 23:41:21 +0200

    add production settings

---
+++ master/prod_settings.py
@@ -0,0 +1,11 @@
+from .settings import *
+
+
+DEBUG = False
+
+TEMPLATE_DEBUG = False # debug template deactivate
+
+SECRET_KEY = 'usn)9gzldaz@%)84myuikf3#38fsqi+ns#sc)=jji#$^0w&pt*'
+
+ALLOWED_HOSTS = ['p8pb.herokuapp.com'] # authorized hostname
+
