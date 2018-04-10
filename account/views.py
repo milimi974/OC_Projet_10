3fcdfde734236a2a121555b482cb79dd923e27b8
solon <milimi974@gmail.com>
2018-04-10 15:48:32 +0200

    Debug account

--- account/views.py
+++ account/views.py
@@ -51,23 +51,8 @@ def logout_view(request):
 
 # register view
 def register_view(request):
-    title = "inscription"
-    form = UserRegisterForm(request.POST or None)
-    # get reference to page to redirect after connection
-    next = request.GET.get('next')
 
-    # Create user if clean form data
-    if form.is_valid():
-        user = form.save(commit=False)
-        pwd = form.cleaned_data.get('password')
-        user.set_password(pwd)
-        user.save()
-        login(request, user)
-        if next:
-            return redirect(next)
-        return redirect("/")
-
-    context = {"form": form, "title": title}
+    context = {"form": '', "title": ''}
     return render(request, 'account/form.html', context)
 
 # user profile view
@@ -77,28 +62,12 @@ def profile_view(request):
 
 @login_required(login_url='/user/login/')
 def list_view(request):
-    # user product substitutions
-    title = "Mes aliments de substitution"
-    #substitutions = Product.all()
-    user_products = UserProduct.objects.all().order_by('id')
 
-    # pagination
-    paginator = Paginator(user_products, 12)
-    page = request.GET.get('page')
-
-    try:
-        user_products = paginator.page(page)
-    except PageNotAnInteger:
-        user_products = paginator.page(1)
-    except EmptyPage:
-        user_products = paginator.page(paginator.num_pages)
-
-    pagination = tools.pagination(user_products, 10)
 
     context = {
-        'user_products': user_products,
-        'title': title,
-        'pagination': pagination
+        'user_products': '',
+        'title': '',
+        'pagination': ''
     }
 
     return render(request, 'account/user_list.html', context)
@@ -106,24 +75,6 @@ def list_view(request):
 
 def save(request):
     data_json = {'status': 'error','message':'Produit déjà dans vos favoris.'}
-    # if user are login
-    if request.user.is_authenticated and request.is_ajax:
-        # extract params from request
-        product_id =  request.POST.get('id')
-        substitution_id =  request.POST.get('sub_id')
-        # if ids exists
-        if product_id and substitution_id:
-            # if substitution product already exist
-            sub = Product.objects.get(pk=substitution_id)
-            if sub:
-                # if product assimilate to user
-                user_product_qs, create = UserProduct.objects.get_or_create(product_id=product_id, user=request.user)
-                # test if relation already exist none create
-                qs = user_product_qs.substitutions.filter(pk=substitution_id)
-                if not qs.exists():
-                    # if no relation add entry
-                    user_product_qs.substitutions.add(sub)
-                    data_json = {'status': 'success', 'message': 'Produit ajouté dans vos favoris.'}
 
     # convert json answer
     data_json = json.dumps(data_json)
