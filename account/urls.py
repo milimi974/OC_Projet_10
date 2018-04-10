75109454d221cf193704e1b568cda324295d79a7
solon <milimi974@gmail.com>
2018-04-10 16:00:12 +0200

    Debug account register view

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 
-from account.views import save, register_view, profile_view, login_view, logout_view, list_view
+from account.views import save_view, register_view, profile_view, login_view, logout_view, list_view
 
 urlpatterns = [
-    url(r'^substitution/$', list_view, name="substitution"),
-    url(r'^save/$', save, name="save_product"),
-    url(r'^register/$', register_view, name="register"),
-    url(r'^profile/$', profile_view, name="profile"),
+    #url(r'^substitution/$', list_view, name="substitution"),
+    #url(r'^save/$', save_view, name="save_product"),
+    #url(r'^register/$', register_view, name="register"),
+    #url(r'^profile/$', profile_view, name="profile"),
     url(r'^login/$', login_view, name="login"),
     url(r'^logout/$', logout_view, name="logout"),
 ]
