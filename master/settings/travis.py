a558dac9b6fccc31e7508b1d9f2d95c2ead1502e
solon <milimi974@gmail.com>
2018-04-15 20:07:31 +0200

    init ci travis

---
+++ master/settings/travis.py
@@ -0,0 +1,13 @@
+from master.settings import *
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
